1b69d24389501aa53a3f9201e80b07fbc41a9c92
Glouww <joao.fgb2003@gmail.com>
2025-11-26 14:01:01 +0000

    New fork, fixed misatkes; task 1-2 completed.

---
+++ src/main/java/com/jpmc/midascore/component/KafkaConsumer.java
@@ -0,0 +1,19 @@
+package com.jpmc.midascore.component;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = "${general.kafka-topic}", groupId = "my-group")
+    public void listen(Transaction transaction) {
+        logger.info("Received Transaction: {}", transaction);
+    }
+}
+
